c44cfd71e135b22b3a48db369f06a82c3e290216
Geoffrey Noel <noel.geoff@gmail.com>
2016-09-17 03:55:30 -0400

    Move git-commit-init.el to .emacs.d

--- emacs.d/git-commit-init.el
+++ emacs.d/git-commit-init.el
@@ -3,25 +3,24 @@
 ;                  _| __/ | | | | | (_| | (__\__ \
 ;                 (_)___|_| |_| |_|\__,_|\___|___/
 ;
-;
 
 (if (< emacs-major-version 25)
     (setq tramp-ssh-controlmaster-options
           "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no"))
 
 ;; Set path to dependencies
-(setq elpa-lisp-dir (expand-file-name "elpa" user-emacs-directory))
-(dolist (project (directory-files elpa-lisp-dir t "\\w+"))
-  (when (file-directory-p project)
-    (add-to-list 'load-path project)))
+(let ((elpa-lisp-dir (expand-file-name "elpa" user-emacs-directory)))
+  (dolist (project (directory-files elpa-lisp-dir t "\\w+"))
+    (when (file-directory-p project)
+      (add-to-list 'load-path project))))
 
 (require 'color-theme)
 (color-theme-initialize)
+(color-theme-charcoal-black)
 
 ;; Setup git-commit mode
 (require 'git-commit)
 (add-hook 'git-commit-mode-hook (lambda()
-                                  (color-theme-charcoal-black)
                                   (ruler-mode)
                                   (flyspell-mode t)))
 ;; Switch to git-commit mode
